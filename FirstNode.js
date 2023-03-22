120a90ff3d9a1e98d13274e5b75cf07ab3851e03
rawatabhay2601 <rawatabhay2601@gmail.com>
2023-03-22 21:28:59 +0530

    ArrowFunctionAndObjectMethod

--- FirstNode.js
+++ FirstNode.js
@@ -1,1 +1,15 @@
-console.log('Hello World!!!');
+const a = 2;
+const b = 5;
+
+const product = (a,b) => {
+    return a*b;
+}
+
+const student = {
+    name : "Abhay",
+    age : 24,
+    greet() {
+        console.log(`Hi! I'm ${this.name}`);
+    }
+}
+student.greet();
