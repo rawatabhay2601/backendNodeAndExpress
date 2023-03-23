c9c9f06f7b962bb84b7eb405a803a13f0866e5dc
rawatabhay2601 <rawatabhay2601@gmail.com>
2023-03-23 18:34:19 +0530

    AddingRoutesToServer

--- FirstServer.js
+++ FirstServer.js
@@ -1,5 +1,25 @@
 const http = require('http');
+const server = http.createServer((req,res) => {
+    if(req.url == '/home'){
 
-const server = http.createServer((req,res) => {console.log('Abhay Rawat')})
-
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<body> Welcome to Home !!!</body>');
+        res.write('</html>');
+    }
+    else if(req.url == '/about'){
+        
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<body> Welcome to About Us page !!!</body>');
+        res.write('</html>');
+    }
+    else if(req.url == '/node'){
+        
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<body> Welcome to my Node Js project!!!</body>');
+        res.write('</html>');
+    }
+})
 server.listen(4000);
