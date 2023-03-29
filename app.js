8f7abe8a44dac024abbf842a0536eebab802155f
rawatabhay2601 <rawatabhay2601@gmail.com>
2023-03-30 00:02:50 +0530

    AddingRoutesAndParsingData

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use( (req,res,next) => {
-    console.log('In the middleware');
-    next();
-})
+ 
+app.use(bodyParser.urlencoded({extended:false}));
 
-app.use( (req,res,next) => {
-    console.log('Next middleware');
+app.use('/add-product',(req,res,next) => {
+    res.send("<form action='/product' method='POST'><input type='text' name='product' placeholder='Product'><input type='number' name='product' placeholder='Product Size'><button type='submit'>Submit</button></input></form>");
+    
+});
+
+app.use('/product', (req,res,next) => {
+    console.log('Product',req.body['product'][0]);
+    console.log('Product-Size',req.body['product'][1]);
+    res.redirect('/');
+});
+
+app.use('/', (req,res,next) => {
     res.send('<h1>Hello from Express.js!!</h1>');
-})
-
+});
 
 app.listen(3000);
