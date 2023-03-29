88008f306d94f772831100c00629b26687566ba7
rawatabhay2601 <rawatabhay2601@gmail.com>
2023-03-29 14:19:16 +0530

    AddingExpress

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-const http = require('http');
-const fs = require('fs');
-let data = '';
-let routes = require('./routes'); 
-console.log(routes.someText);
-const server = http.createServer(routes.handler);
-server.listen(4000);
+const express = require('express');
+const app = express();
+
+app.use( (req,res,next) => {
+    console.log('In the middleware');
+    next();
+})
+
+app.use( (req,res,next) => {
+    console.log('Next middleware');
+    res.send('<h1>Hello from Express.js!!</h1>');
+})
+
+
+app.listen(3000);
